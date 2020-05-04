86d384f1a4469ceaf21ac0751e9622db17640b6a
kezike <kezike13@gmail.com>
2020-05-04 00:41:48 -0400

    Updates

---
+++ precache-manifest.5968ad83abca93a85e3b955cc40dd59d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2cfdc6445f8c120a0b1c9ca04d9dc12",
+    "url": "/sample-react-gh-pages/index.html"
+  },
+  {
+    "revision": "a13b63c4097846ac4c8c",
+    "url": "/sample-react-gh-pages/static/css/main.75060aac.chunk.css"
+  },
+  {
+    "revision": "2477239f2b297f5c60bb",
+    "url": "/sample-react-gh-pages/static/js/2.071193dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample-react-gh-pages/static/js/2.071193dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a13b63c4097846ac4c8c",
+    "url": "/sample-react-gh-pages/static/js/main.489b50ad.chunk.js"
+  },
+  {
+    "revision": "91ab40297240366255da",
+    "url": "/sample-react-gh-pages/static/js/runtime-main.92b8c7e0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
